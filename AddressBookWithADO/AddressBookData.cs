376be728c8260e1fb45af6deef1ed832a1c80d05
Sangram <sangram15.behera@gmail.com>
2022-06-26 02:25:22 +0530

    Create table inside a database

--- AddressBookWithADO/AddressBookData.cs
+++ AddressBookWithADO/AddressBookData.cs
@@ -15,7 +15,7 @@ namespace AddressBook_ADO.NET
             {
                 SqlConnection Connection = new SqlConnection(@"Data Source=BridgeLabz; Initial Catalog =master; Integrated Security = True;");
                 Connection.Open();
-                SqlCommand command = new SqlCommand("Create database AddressbookForADO;", Connection);
+                SqlCommand command = new SqlCommand("Create database AddressbookADO;", Connection);
                 command.ExecuteNonQuery();
                 Console.WriteLine("AddressbookService Database created successfully!");
                 Connection.Close();
@@ -23,6 +23,22 @@ namespace AddressBook_ADO.NET
             catch (Exception e)
             {
                 Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+        public void CreateTables()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=BridgeLabz; Initial Catalog =AddressbookADO; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand command = new SqlCommand("Create table AddressBook(id int identity(1,1)primary key,FirstName varchar(200),LastName varchar(200),Address varchar(200), City varchar(200), State varchar(200), Zip varchar(200), PhoneNumber varchar(50), Email varchar(200)); ", Connection);
+                command.ExecuteNonQuery();
+                Console.WriteLine("AddressBook table has been  created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating table:" + e.Message + "\t");
             }
         }
 
--- AddressBookWithADO/Program.cs
+++ AddressBookWithADO/Program.cs
@@ -5,3 +5,18 @@ Console.WriteLine("Welcome to AddressBook ADO.NET!");
 
 AddressBookData addressBookData = new AddressBookData();
 addressBookData.Create_Database();
+
+Console.WriteLine("Select option\n1)Create AddrssBookServiceDatabase\n2)Create AddressBookTable");
+int op = Convert.ToInt16(Console.ReadLine());
+switch (op)
+{
+    case 1:
+        addressBookData.Create_Database();
+        break;
+    case 2:
+        addressBookData.CreateTables();
+        break;
+    default:
+        Console.WriteLine("Please choose the correct option!");
+        break;
+}
